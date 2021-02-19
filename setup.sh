ee9a6db17b5dd0b7fc9d4b553488c71d8ab591f1
Doug Richardson <git@rekt.email>
2021-02-19 12:21:19 -0800

    Add script to integrate gpg4win upstream in a cronjob.

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
+
+DEPLOY_KEY="$HOME/.ssh/gpg4win-deploy-key"
+
+cat <<EOF
+Add this to $HOME/.ssh/config:
+
+Host github-gpg4win-deploy
+        Hostname github.com
+	IdentityFile /home/doug/.ssh/gpg4win-deploy-key
+EOF
+
+read -p 'Press Enter when you have done this' CONFIRM
+
+echo Setting up deploy keys
+
+if [[ -f "$DEPLOY_KEY" ]]; then
+	echo "$DEPLOY_KEY already exists. Remove it if you want to re-run setup."
+	exit 1
+fi
+
+ssh-keygen -t ed25519 -f "$DEPLOY_KEY" -N ''
+
+cat <<EOF
+Copy this public deploy key to:
+  https://github.com/drichardson/gpg4win/settings/keys/new
+Grant the key write access.
+==== COPY BETWEEN THIS ====
+EOF
+cat $DEPLOY_KEY.pub
+cat <<EOF
+==== COPY BETWEEN THIS ====
+EOF
+
+echo Copy the key above to https://github.com/drichardson/gpg4win/settings/keys/new
+read -p 'Press Enter when you have done this.' CONFIRM
+
+echo Configure Repository
+cd "$SCRIPTDIR"
+git clone git@github-gpg4win-deploy:drichardson/gpg4win.git
+cd gpg4win
+git remote add upstream git://git.gnupg.org/gpg4win.git
+
+echo Setup complete. You can now run update-master.sh.
---
+++ update-master.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
+
+cd "$SCRIPTDIR/gpg4win"
+git fetch --all
+git merge --ff-only upstream/master
+git push origin master
