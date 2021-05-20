55c2f32727dad2c00f40c635015b2e132f8b4b5a
Doug Richardson <git@rekt.email>
2021-05-20 11:09:32 -0700

    rebase against upstream master

--- update-master.sh
+++ update-master.sh
@@ -6,6 +6,6 @@ SCRIPTDIR=$(dirname "$0")
 SCRIPTDIR=$(readlink -f "$SCRIPTDIR")
 
 cd "$SCRIPTDIR/gpg4win"
-git fetch --all
-git merge upstream/master -m "merge upstream/master"
-git push -q origin master
+git fetch --all -p
+git rebase upstream/master
+git push -qf origin master
